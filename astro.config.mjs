75e678291b5f56dec93e522b61f6ad10ec76abb5
ActualXR <admin@actualxr.com>
2026-03-24 22:38:42 +0000

    "Bulletproof" simplified version

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,8 @@ import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 import db from '@astrojs/db';
 
-// https://astro.build/config
 export default defineConfig({
-  // Order matters: put db() first
-  integrations: [db(), netlify()],
   output: 'server',
   adapter: netlify(),
+  integrations: [db()],
 });
